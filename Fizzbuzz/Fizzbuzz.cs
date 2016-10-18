008e4f75cc1bc975e83d072fc6a88d87a4a68d44
Nicholas Underwood <underwnm@gmail.com>
2016-10-18 15:06:53 -0500

    Fizzbuzz
    Added fizzbuzz backup

---
+++ Fizzbuzz/Fizzbuzz.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzbuzzProgram
+{
+    class Fizzbuzz
+    {
+
+        public void PrintZeroToOneHundred()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                string output;
+                bool CanBeDividedByThree = CanBeDividedBy(i, 3);
+                bool CanBeDividedByFive = CanBeDividedBy(i, 5);
+
+                if (CanBeDividedByFive && CanBeDividedByThree)
+                {
+                    output = "fizzbuzz";
+                } else if (CanBeDividedByThree)
+                {
+                    output = "fizz";
+                } else if (CanBeDividedByFive)
+                {
+                    output = "buzz";
+                } else
+                {
+                    output = i.ToString();
+                }
+                Console.WriteLine(output);
+            }
+        }
+        public bool CanBeDividedBy(int indexNumber, int targetNumber)
+        {
+            return (indexNumber % targetNumber) == 0;
+        }
+    }
+}
---
+++ Fizzbuzz/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzbuzzProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fizzbuzz current = new Fizzbuzz();
+            current.PrintZeroToOneHundred();
+
+            Console.ReadKey();
+        }
+    }
+}
